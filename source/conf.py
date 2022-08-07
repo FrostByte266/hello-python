676e5c632ff37ce1a8e07669fee4e4826ddbf464
William Grigor <bjgrigor@gmail.com>
2022-08-07 18:02:29 -0500

    Enable GH page extension

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = []
